891b2068a6fa5ac548d9747f4a714def7077e259
lacykaltgr <freundl0509@gmail.com>
2023-05-18 23:36:28 +0200

    blockchain

--- src/popup/pages/index.vue
+++ src/popup/pages/index.vue
@@ -2,17 +2,19 @@
 <template>
   <img class="logo" src="/src/assets/logo.png" alt="Logo">
   <span>
-      <button @click="changePage">your metaverse</button>
-      <button @click="changePage">your metaverse</button>
+      <button class="page-button" @click="changePage">your metaverse</button>
+      <button class="page-button" @click="changePage">your metaverse</button>
   </span>
-  <ScrollView></ScrollView>
+  <timeline></timeline>
+  <recommend></recommend>
 </template>
 
 <script>
-import ScrollView from "~/popup/pages/ScrollView.vue";
+import Timeline from "~/popup/pages/Timeline.vue";
+import Recommend from "~/popup/pages/Recommend.vue";
 
 export default {
-  components: {ScrollView},
+  components: {Timeline, Recommend},
 }
 </script>
 
@@ -25,4 +27,12 @@ export default {
   top: 10px; /* Adjust the top position as needed */
   left: 10px; /* Adjust the left position as needed */
 }
+
+.page-button {
+  width: 100px;
+  height: 30px;
+  transition: width 0.5s, height 0.5s;
+  padding: 2px;
+}
+
 </style>
