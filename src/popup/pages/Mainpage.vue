9790b5de1c6f7a871267e20c0da12af28376d975
lacykaltgr <freundl0509@gmail.com>
2023-05-19 00:41:16 +0200

    mainpage

---
+++ src/popup/pages/Mainpage.vue
@@ -0,0 +1,21 @@
+
+<template>
+  <timeline></timeline>
+  <recommend></recommend>
+</template>
+
+<script>
+import Timeline from "~/popup/pages/Timeline.vue";
+import Recommend from "~/popup/pages/Recommend.vue";
+import {defineComponent} from "vue";
+
+export default defineComponent({
+  components: {Timeline, Recommend}
+})
+</script>
+
+
+
+<style scoped>
+
+</style>
--- src/popup/pages/index.vue
+++ src/popup/pages/index.vue
@@ -1,28 +1,42 @@
 
 <template>
   <img class="logo" src="/src/assets/logo.png" alt="Logo">
-  <span>
-      <button class="page-button" @click="changePage">your metaverse</button>
-      <button class="page-button" @click="changePage">your metaverse</button>
-  </span>
-  <timeline></timeline>
-  <recommend></recommend>
+  <div class="page-buttons">
+    <button class="page-button" @click="changePage('mainpage')">Recommend</button>
+    <button class="page-button" @click="changePage('objects')">Your metaverse</button>
+  </div>
+  <div class="page">
+    <component :is="currentPage"></component>
+  </div>
+
 </template>
 
 <script>
 import Timeline from "~/popup/pages/Timeline.vue";
 import Recommend from "~/popup/pages/Recommend.vue";
+import Mainpage from "~/popup/pages/Mainpage.vue";
+import Objects from "~/popup/pages/Objects.vue";
 
 export default {
-  components: {Timeline, Recommend},
-}
+  components: { Timeline, Recommend, Objects, Mainpage },
+  data() {
+    return {
+      currentPage: "timeline"
+    };
+  },
+  methods: {
+    changePage(page) {
+      this.currentPage = page;
+    }
+  }
+};
 </script>
 
 
 
 <style scoped>
 .logo {
-  width: 50px; /* Adjust the width as needed */
+  width: 30px; /* Adjust the width as needed */
   position: absolute;
   top: 10px; /* Adjust the top position as needed */
   left: 10px; /* Adjust the left position as needed */
@@ -33,6 +47,23 @@ export default {
   height: 30px;
   transition: width 0.5s, height 0.5s;
   padding: 2px;
+}
+
+.page-buttons {
+  position: absolute;
+  top: 50px;
+  left: 0;
+  right: 0;
+  margin: 0 auto;
+  width: 200px; /* adjust as needed */
+}
+
+.page {
+  position: relative;
+  top: 30px;
+  left: 0;
+  margin: 50px;
+  bottom: 100px;
 }
 
 </style>
